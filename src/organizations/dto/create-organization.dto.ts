996bcd3b8fbb9cbc51bbd171776ed726a8a54760
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-15 17:54:09 -0600

    updated create organization dto
    updated create organization dto

--- src/organizations/dto/create-organization.dto.ts
+++ src/organizations/dto/create-organization.dto.ts
@@ -1,4 +1,27 @@
+class CreateMetricsDto {
+  coverage: number;
+  bugs: number;
+  vulnerabilities: number;
+  hotspot: number;
+  code_smells: number;
+}
+
+class CreateRepositoryDto {
+  name: string;
+  state: string;
+  status: string;
+  metrics: CreateMetricsDto;
+}
+
+
+class CreateTribeDto {
+  name: string;
+  status: number;
+  repositories: [CreateRepositoryDto];
+}
+
 export class CreateOrganizationDto {
   name: string;
   status: number;
+  tribes: [CreateTribeDto];
 }
