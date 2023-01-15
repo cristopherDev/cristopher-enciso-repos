1b831b0b4c7d18c73ebd4966494c129a478ed6a7
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-15 07:13:29 -0600

    added prisma module and service to organizations
    added prisma module and service to organizations

--- src/organizations/organizations.module.ts
+++ src/organizations/organizations.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { OrganizationsController } from './organizations.controller';
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
   controllers: [OrganizationsController],
-  providers: [OrganizationsService]
+  providers: [OrganizationsService],
+  imports: [PrismaModule]
 })
 export class OrganizationsModule {}
--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class OrganizationsService {
+  constructor(private prisma: PrismaService) {}
+  
   create(createOrganizationDto: CreateOrganizationDto) {
     return 'This action adds a new organization';
   }
