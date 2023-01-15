2e4f39d383f9bee8d687d87314c47a3b4e22a73d
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-15 07:08:53 -0600

    added organizations resource
    added organizations resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, OrganizationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
