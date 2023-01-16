9d828da60fe7a08ab27547012c2e8ee69b2156b6
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-15 23:17:21 -0600

    Update create-organization.dto.ts
    added swagger api property

--- src/organizations/dto/create-organization.dto.ts
+++ src/organizations/dto/create-organization.dto.ts
@@ -9,6 +9,7 @@ import {
   ValidateNested,
   IsEnum,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 type RepositoryStatusTypes = 'A' | 'I';
 type RepositoryStatesTypes = 'E' | 'D' | 'A';
@@ -17,49 +18,61 @@ const RepositoryStatus: RepositoryStatusTypes[] = ['A', 'I'];
 const RepositoryStates: RepositoryStatesTypes[] = ['E', 'D', 'A'];
 
 class CreateMetricsDto {
+  @ApiProperty()
   @IsNumber()
   coverage: number;
 
+  @ApiProperty()
   @IsNumber()
   bugs: number;
 
+  @ApiProperty()
   @IsNumber()
   vulnerabilities: number;
 
+  @ApiProperty()
   @IsNumber()
   hotspot: number;
 
+  @ApiProperty()
   @IsNumber()
   code_smells: number;
 }
 
 class CreateRepositoryDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsEnum(RepositoryStates, { message: () => 'state debe de ser E o D o A' })
   state: RepositoryStatesTypes;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsEnum(RepositoryStatus, { message: () => 'status debe de ser A o I' })
   status: RepositoryStatusTypes;
 
+  @ApiProperty({ type: CreateMetricsDto })
   @IsObject()
   metrics: CreateMetricsDto;
 }
 
 class CreateTribeDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsNumber()
   status: number;
 
+  @ApiProperty({isArray: true, type: CreateRepositoryDto})
   @IsArray()
   @ArrayMinSize(1)
   @ValidateNested({ each: true })
@@ -68,13 +81,16 @@ class CreateTribeDto {
 }
 
 export class CreateOrganizationDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsNumber()
   status: number;
 
+  @ApiProperty({isArray: true, type: CreateTribeDto})
   @IsArray()
   @ArrayMinSize(1)
   @ValidateNested({ each: true })
