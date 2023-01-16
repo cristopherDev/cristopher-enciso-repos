546d2d086ba93c8384f3f2fc27196a4417b6fce2
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-16 11:14:48 -0600

    Update tribes.controller.ts
    added endpoint to download csv file report of repositories

--- src/tribes/tribes.controller.ts
+++ src/tribes/tribes.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  NotFoundException,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { TribesService } from './tribes.service';
+import { json2csvAsync } from 'json-2-csv';
+import type { Response } from 'express';
+
+const notFoundText: string = `La Tribu no se encuentra registrada`;
+const notCoverageText: string = `La Tribu no tiene repositorios que cumplan con la cobertura necesaria`;
 
 @Controller('tribes')
 export class TribesController {
@@ -7,14 +19,28 @@ export class TribesController {
 
   @Get(':id/repositories')
   async findRepositories(@Param('id') id: string): Promise<{} | string> {
-    const notFoundText = `La Tribu ${id} no se encuentra registrada`;
-    const notCoverageText = `La Tribu ${id} no tiene repositorios que cumplan con la cobertura necesaria`;
     const tribe = await this.tribesService.findRepositories(+id);
 
     if (!tribe) throw new NotFoundException(notFoundText);
     if (!Object.keys(tribe).length)
       throw new NotFoundException(notCoverageText);
 
-    return tribe;
+    return { repositories: tribe };
+  }
+
+  @Get(':id/repositories/report')
+  async exportRepositories(@Param('id') id: string, @Res() res: Response) {
+    const tribe = await this.tribesService.findRepositories(+id);
+
+    if (!tribe) throw new NotFoundException(notFoundText);
+    if (!Object.keys(tribe).length)
+      throw new NotFoundException(notCoverageText);
+
+    const csv = await json2csvAsync(tribe);
+
+    res.header('Content-Type', 'text/csv');
+    res.attachment('repositories.csv');
+
+    return res.send(csv);
   }
 }
