5900f62660f3cf9bc92a769e9a4104a88b915140
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-15 23:22:18 -0600

    Update update-organization.dto.ts
    added class validator and map api property

--- src/organizations/dto/update-organization.dto.ts
+++ src/organizations/dto/update-organization.dto.ts
@@ -1,4 +1,12 @@
+import { IsNumber, IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateOrganizationDto {
+    @ApiProperty()
+    @IsString()
     name: string;
+
+    @ApiProperty()
+    @IsNumber()
     status: number;
 }
