e9898c39908cb19b2c2dbf9d5de958d98bb6c5af
Cristopher Paolo Enciso Torres <cristopher.paolo.et@gmail.com>
2023-01-15 23:16:51 -0600

    added swagger to main config
    added swagger to main config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle('Ejercicio práctico')
+    .setDescription('BackEnd API description')
+    .setVersion('1.0')
+    .build();
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
